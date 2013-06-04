1f843b48a1d94b27efc65b7fb70dd13192a7f9db
sztorman <michal.uziak@gmail.com>
2013-06-05 01:16:59 +0200

    Wyglada na to, ze dziala

---
+++ src/dupa.java
@@ -0,0 +1,14 @@
+
+public class dupa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nussinov nus = new nussinov("GGCCAA");
+		
+		
+	}
+
+}
